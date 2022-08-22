c93462e621047f1183e05333df0bd4c13e919888
James-Andrew R. Sarmiento <sarmientoj24@gmail.com>
2022-08-22 15:51:45 +0800

    Change multicrop dataset to use Albumentations

--- src/multicropdataset.py
+++ src/multicropdataset.py
@@ -10,7 +10,8 @@ from logging import getLogger
 from PIL import ImageFilter
 import numpy as np
 import torchvision.datasets as datasets
-import torchvision.transforms as transforms
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
 
 logger = getLogger()
 
@@ -38,57 +39,31 @@ class MultiCropDataset(datasets.ImageFolder):
         mean = [0.485, 0.456, 0.406]
         std = [0.228, 0.224, 0.225]
         trans = []
+        s = 1.0
         for i in range(len(size_crops)):
-            randomresizedcrop = transforms.RandomResizedCrop(
-                size_crops[i],
-                scale=(min_scale_crops[i], max_scale_crops[i]),
+            randomresizedcrop = A.RandomResizedCrop(
+                size_crops[i], size_crops[i], scale=(min_scale_crops[i], max_scale_crops[i])
             )
-            trans.extend([transforms.Compose([
+
+            main_transform = [
                 randomresizedcrop,
-                transforms.RandomHorizontalFlip(p=0.5),
-                transforms.Compose(color_transform),
-                transforms.ToTensor(),
-                transforms.Normalize(mean=mean, std=std)])
-            ] * nmb_crops[i])
+                # Color Jitter
+                A.ColorJitter(0.8*s, 0.8*s, 0.8*s, 0.2*s, p=0.8),
+                A.ToGray(p=0.2),
+                A.GaussianBlur(blur_limit=5, sigma_limit=(0.1, 2.), p=0.5), 
+                A.HorizontalFlip(),
+                A.ToFloat(255),
+                A.Normalize(mean=mean, std=std, max_pixel_value=1),
+                ToTensorV2(),
+            ]
+
+            trans.extend([A.Compose(main_transform)] * nmb_crops[i])
         self.trans = trans
 
     def __getitem__(self, index):
         path, _ = self.samples[index]
         image = self.loader(path)
-        multi_crops = list(map(lambda trans: trans(image), self.trans))
+        multi_crops = list(map(lambda trans: trans(np.array(image)), self.trans))
         if self.return_index:
             return index, multi_crops
         return multi_crops
-
-
-class PILRandomGaussianBlur(object):
-    """
-    Apply Gaussian Blur to the PIL image. Take the radius and probability of
-    application as the parameter.
-    This transform was used in SimCLR - https://arxiv.org/abs/2002.05709
-    """
-
-    def __init__(self, p=0.5, radius_min=0.1, radius_max=2.):
-        self.prob = p
-        self.radius_min = radius_min
-        self.radius_max = radius_max
-
-    def __call__(self, img):
-        do_it = np.random.rand() <= self.prob
-        if not do_it:
-            return img
-
-        return img.filter(
-            ImageFilter.GaussianBlur(
-                radius=random.uniform(self.radius_min, self.radius_max)
-            )
-        )
-
-
-def get_color_distortion(s=1.0):
-    # s is the strength of color distortion.
-    color_jitter = transforms.ColorJitter(0.8*s, 0.8*s, 0.8*s, 0.2*s)
-    rnd_color_jitter = transforms.RandomApply([color_jitter], p=0.8)
-    rnd_gray = transforms.RandomGrayscale(p=0.2)
-    color_distort = transforms.Compose([rnd_color_jitter, rnd_gray])
-    return color_distort
